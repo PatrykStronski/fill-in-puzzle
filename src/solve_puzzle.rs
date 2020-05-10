2c35cb7002b395c66d02ab28a7caa502c26edca7
Patryk Stronski <p.stronski13@gmail.com>
2020-05-10 18:08:21 +0200

    doesn't use all possibilities

--- src/solve_puzzle.rs
+++ src/solve_puzzle.rs
@@ -1,13 +1,12 @@
 use crate::puzzle::Puzzle;
 
-fn remove_from_lexicone(lexicone: &mut Vec<String>, word:String) -> bool {
+fn find_index(lexicone: &Vec<String>, word:String) -> i16 {
     for i in 0..lexicone.len() {
         if lexicone[i] == word {
-            lexicone.remove(i);
-            return true;
+            return i as i16;
         }
     }
-    return false;
+    return -1;
 }
 
 fn backtrack_step(puz: &mut Puzzle, lexicone: &mut Vec<String>, x: usize, y: usize) -> bool {
@@ -16,26 +15,38 @@ fn backtrack_step(puz: &mut Puzzle, lexicone: &mut Vec<String>, x: usize, y: usi
     }
     if puz.exceed_bonds(x, y) {
         if puz.y_critical(y) {
+            println!("{}", puz.print_current_board());
             return puz.validate_puzzle();
         }
-        return backtrack_step(puz,lexicone, 0, y+1)
+        return backtrack_step(puz,lexicone, 0, y+1);
     }
-    for word in lexicone.to_vec() {
+    let mut lex_len = lexicone.len();
+    let mut index: usize = 0;
+    while index < lex_len {
+        let word = lexicone[index].to_string();
         if puz.write_word(word.to_string(), x, y, true) {
-            if !remove_from_lexicone(lexicone, word.to_string()) {
+            let to_remove = find_index(lexicone, word.to_string());
+            if to_remove == -1 {
                 continue;
             }
+            lexicone.remove(to_remove as usize);
+            lex_len -= 1;
             let mut x_shifted = x + word.len();
             if puz.get_element(x_shifted, y) == '#' {
                 x_shifted += 1;
             }
             if x_shifted >= puz.width {
-                return backtrack_step(puz, &mut lexicone.to_vec(), 0, y+1);
+                if backtrack_step(puz, &mut lexicone.to_vec(), 0, y+1) {
+                    return true;
+                }
             }
-            return backtrack_step(puz, &mut lexicone.to_vec(), x_shifted, y);
+            if backtrack_step(puz, &mut lexicone.to_vec(), x_shifted, y) {
+                return true;
+            }
         }
+        index += 1;
     }
-    return puz.validate_puzzle();
+    return false;
 }
 
 fn backtrack(puz: &mut Puzzle) -> bool {
