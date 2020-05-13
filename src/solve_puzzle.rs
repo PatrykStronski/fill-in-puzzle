3e9896b3ed4831004f814377d4345c0393e55fa4
Patryk Stronski <p.stronski13@gmail.com>
2020-05-14 00:23:31 +0200

    one operation thrown away

--- src/solve_puzzle.rs
+++ src/solve_puzzle.rs
@@ -1,9 +1,9 @@
 use crate::puzzle::Puzzle;
 
-fn get_new_lexicone(lexicone: &Vec<String>, word: String) -> Vec<String> {
+fn get_new_lexicone(lexicone: &Vec<String>, word: &String) -> Vec<String> {
     let mut new_lexicone = lexicone.to_vec();
     for i in 0..new_lexicone.len() {
-        if new_lexicone[i] == word {
+        if new_lexicone[i] == *word {
             new_lexicone.remove(i);
             return new_lexicone;
         }
@@ -12,7 +12,7 @@ fn get_new_lexicone(lexicone: &Vec<String>, word: String) -> Vec<String> {
 }
 
 fn backtrack_step(puz: &mut Puzzle, lexicone: &mut Vec<String>, x: usize, y: usize) -> bool {
-    println!("x: {}, y: {}", x, y);
+    // println!("x: {}, y: {}", x, y);
     if puz.exceed_bonds(x, y) {
         return puz.validate_puzzle();
     }
@@ -24,7 +24,7 @@ fn backtrack_step(puz: &mut Puzzle, lexicone: &mut Vec<String>, x: usize, y: usi
     }
     for word in lexicone.to_vec() {
         if puz.write_word(word.to_string(), x, y, true) {
-            let mut lexicone_new = get_new_lexicone(lexicone, word.to_string());
+            let mut lexicone_new = get_new_lexicone(lexicone, &word);
             let mut x_shifted = x + word.len();
             if backtrack_step(puz, &mut lexicone_new.to_vec(), x_shifted, y) {
                 return true;
